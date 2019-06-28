78678935902d4cd5161302b2f4d308bed0977c8c
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-06-28 17:37:13 -0400

    Added styled-components and google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
-    <div>Hi</div>
+    <>
+      <h1>Slicelnnne</h1>
+      <GlobalStyle />
+        <div>Hello pizzzza</div>
+    </>
   );
 }
 
