a17109f1094bfbfdd38a0cb72a8409f89832e1dc
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2019-06-28 18:08:21 -0400

    Added images and Banner

---
+++ src/Banner/Banner.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+  position: relative;
+  top: 49px;
+  height: 200px;
+  background-image: url("img/banner.jpeg");
+  background-position: center;
+  background-size: cover;
+`;
